247c3cbf21286e839f62fa16fb37fd2974e7a6a4
adityaharitama <adityaharitama@gmail.com>
2021-10-04 17:35:24 +0700

    change livedata

--- app/src/main/java/com/example/livedataapss/MainActivityViewModel.kt
+++ app/src/main/java/com/example/livedataapss/MainActivityViewModel.kt
@@ -1,14 +1,16 @@
 package com.example.livedataapss
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel : ViewModel() {
-    var calcucationValue = MutableLiveData<Int>()
+    private val _calcucationValue = MutableLiveData<Int>()
+    val calcucationValue: LiveData<Int> get() = _calcucationValue
 
     fun calculateValue(firstValue: String, secondValue: String) {
         val firstVal: Int = if (firstValue.isEmpty()) 0 else firstValue.toInt()
         val secondVal: Int = if (secondValue.isEmpty()) 0 else secondValue.toInt()
-        calcucationValue.value = firstVal + secondVal
+        _calcucationValue.value = firstVal + secondVal
     }
 }
